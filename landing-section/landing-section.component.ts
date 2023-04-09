826114b2ade251baf22cb7deb20a898bfffde77b
Mazin Fouad <m.fouad@live.de>
2023-04-09 18:45:19 +0200

    models added

--- landing-section/landing-section.component.ts
+++ landing-section/landing-section.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { StoreData } from 'src/app/models/viewModels/store-data';
 
 @Component({
   selector: 'app-landing-section',
   templateUrl: './landing-section.component.html',
-  styleUrls: ['./landing-section.component.scss']
+  styleUrls: ['./landing-section.component.scss'],
 })
 export class LandingSectionComponent {
+  storeInfo: StoreData;
 
+  constructor() {
+    this.storeInfo = new StoreData('shopIN', 'https://fakeimg.pl/300/', ['Karlsruhe', ' Bruchsal', ' Heidlberg']);
+  }
 }
